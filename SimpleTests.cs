23e957a4539abdc3c984d811f19ca448d154263f
Daimon Roesslein <daimonroesslein74@gmail.com>
2023-02-03 15:06:35 -0600

    Initial commit

---
+++ SimpleTests.cs
@@ -0,0 +1,16 @@
+namespace HunterDaimonTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
